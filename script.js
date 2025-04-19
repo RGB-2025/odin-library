3b2ffa27563d9d1c4c7078f5cbfa9d62cedf2396
RGB-2025 <205908174+RGB-2025@users.noreply.github.com>
2025-04-19 17:40:22 +0800

    Create "Create Book" button with functionality
    Not sure though how the dialog animation works, but it does the job of
    fading it.

--- script.js
+++ script.js
@@ -30,7 +30,13 @@ let addBookToLibrary = (title, author, pages, read) => myLibrary.push(new Book(t
 */
 
 function displayBooks() {
+    let bookContainer = document.getElementById('book-container');
+
+    // Remove cards
+    if (bookContainer.querySelector('.card')) {bookContainer.querySelectorAll('.card').forEach(element => element.remove())}
+
     myLibrary.forEach(book => {
+        // Creating cards
         let card = document.createElement('div');
         let title = document.createElement('h1');
         let details = document.createElement('div');
@@ -40,13 +46,13 @@ function displayBooks() {
 
         card.className = 'card';
         details.className = 'details';
-        read.setAttribute('data-read', book.read);
+        read.setAttribute('data-read', book.read ? 'true' : 'false');
 
         [title, authorText, pagesText, read].forEach((element, i) => element.textContent = book.info()[i]);
         
         details.append(authorText, pagesText);
         card.append(title, details, read);
-        document.getElementById('book-container').appendChild(card);
+        bookContainer.appendChild(card);
     })
 }
 
@@ -54,3 +60,36 @@ addBookToLibrary('Book', 'Author', 256, false);
 addBookToLibrary('Another Book', 'Another Author but Has a Really Long Name', 1, true);
 
 displayBooks();
+
+let dialog = document.getElementById('new-book-dialog');
+let newBookForm = document.getElementById('new-book-form');
+let createBtn = document.getElementById('create');
+
+createBtn.addEventListener('click', () => {
+    dialog.showModal();
+})
+
+newBookForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    dialog.close();
+
+    let bookDetails = {}
+
+    var formData = new FormData(newBookForm);
+    formData.read = document.getElementById('read').checked
+
+    for (const [key, value] of formData) {
+        bookDetails[key] ??= value || null;
+        console.log(key, value)
+    }
+
+    addBookToLibrary(
+        bookDetails.title,
+        bookDetails.author,
+        bookDetails.pages,
+        bookDetails.read
+    );
+
+    displayBooks();
+})
+
